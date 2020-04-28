15cfa45ef1eac7ee33045e0034f0c5807734a07c
M3IY0U <timo.nock@stud.h-da.de>
2020-04-28 13:09:36 +0200

    Fixed Background Difference, completed first lab assignment

--- video_processing/Program.cs
+++ video_processing/Program.cs
@@ -5,54 +5,53 @@ using OpenCvSharp;
 
 namespace video_processing
 {
-    class Program
+    internal static class Program
     {
-        static void Main()
+        private const int BlurHighPass = 15;
+        private const int BlurSmoothing = 6;
+        private const int Threshold = 10;
+
+        private static void Main()
         {
             var capture = new VideoCapture("mt_camera_raw.AVI");
-            var background = Cv2.ImRead("1.jpg");
-
-            var blobList = new List<Blob>();
-            var frameCounter = 0;
-            
             var sleepTime = (int) Math.Round(1000 / capture.Fps);
+            var blobList = new List<Blob>();
+            var background = new Mat();
+            var frameCounter = 0;
+
             using (var window = new Window("Video Processing"))
             {
                 var original = new Mat();
                 while (true)
                 {
                     capture.Read(original);
+
+                    if (frameCounter == 0)
+                        background = original.Clone();
+
                     frameCounter++;
 
                     if (original.Empty())
                         break;
 
                     var result = new Mat();
-
                     var blurred = new Mat();
 
                     Cv2.Absdiff(original, background, result);
-                    Cv2.Blur(result, blurred, new Size(15, 15));
+                    Cv2.Blur(result, blurred, new Size(BlurHighPass, BlurHighPass));
                     Cv2.Absdiff(result, blurred, result);
-                    Cv2.Blur(result, result, new Size(6, 6));
+                    Cv2.Blur(result, result, new Size(BlurSmoothing, BlurSmoothing));
                     Cv2.CvtColor(result, result, ColorConversionCodes.BGR2GRAY);
-                    Cv2.Threshold(result, result, 10, 255, ThresholdTypes.Binary);
-
-                    // var erodeElement = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(6, 6));
-                    // var dilateElement = Cv2.GetStructuringElement(MorphShapes.Rect, new Size(10, 10));
-
-                    Cv2.Erode(result, result, new Mat());
-                    Cv2.Dilate(result, result, new Mat(), iterations: 3);
+                    Cv2.Threshold(result, result, Threshold, 255, ThresholdTypes.Binary);
 
                     Cv2.FindContours(result, out var contours, out var hierarchy, RetrievalModes.CComp,
                         ContourApproximationModes.ApproxSimple);
-                    
-                    
-                    var x = hierarchy.Select(hierarchyIndex => hierarchyIndex.ToVec4i()).ToList();
 
-                    if (x.Count > 0)
+                    var vectorList = hierarchy.Select(hierarchyIndex => hierarchyIndex.ToVec4i()).ToList();
+
+                    if (vectorList.Count > 0)
                     {
-                        for (var idx = 0; idx >= 0; idx = x[idx][0])
+                        for (var idx = 0; idx >= 0; idx = vectorList[idx][0])
                         {
                             if (Cv2.ContourArea(contours[idx]) < 30 || contours[idx].Length <= 4) continue;
                             Cv2.Ellipse(original, Cv2.FitEllipse(contours[idx]), Scalar.Blue);
@@ -64,10 +63,11 @@ namespace video_processing
                     }
 
                     window.ShowImage(original);
-                    if (Cv2.WaitKey(sleepTime) == 27)
+                    if (Cv2.WaitKey(sleepTime / 4) == 27)
                         break;
                 }
             }
+
             Console.WriteLine($"{blobList.Count} entries in the list");
         }
     }
